0520d365acf6d23128244185ed5a3d6be205c9ac
Xavier <91884588+alpazified@users.noreply.github.com>
2022-10-30 01:24:59 -0400

    feat: rewrote index file

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-require('dotenv').config()
+require('dotenv').config();
 const { Client, GatewayIntentBits, Partials, ActivityType, Collection } = require('discord.js');
 const fs = require('fs');
-// const { init } = require('./database/mongoose');
+const { init } = require('./database/mongoose');
 
 const client = new Client({
     intents: [
@@ -9,62 +9,60 @@ const client = new Client({
         GatewayIntentBits.GuildBans,
         GatewayIntentBits.GuildMembers
     ],
-    allowedMentions: {
-        repliedUser: false,
-        parse: []
-    },
     partials: [
-        Partials.User,
-        Partials.GuildMember
+        Partials.GuildMember,
+        Partials.User
     ],
+    allowedMentions: {
+        parse: [],
+        repliedUser: false
+    },
+    ws: { properties: { browser: 'Discord iOS' } },
     presence: {
         activities: [{
             type: ActivityType.Listening,
-            name: "among us bass boosted",
+            name: 'among us bass boosted remix lullabies'
         }],
         status: 'online'
-    },
-    ws: { properties: [{ browser: "Discord iOS" }] }
+    }
 });
 
+// Defines the variables for commands, buttons and modals
 client.slashCommands = new Collection();
 client.buttons = new Collection();
 client.modals = new Collection();
 
-module.exports = { client };
-
+// Gets the event files
 const eventFiles = fs.readdirSync('./events')
     .filter((file) => file.endsWith('.js'));
+
+// Sets the event files and runs them
 for (const file of eventFiles) {
     const event = require('./events/' + file)
-    if (!event.disabled) {
-        if (event.once) {
-            client.once(event.name, (...args) => event.run(...args, client));
-        } else {
-            client.on(event.name, (...args) => event.run(...args, client));
-        }
-    }
-};
-
-const slashCommands = fs.readdirSync('./commands')
-
-for (const module of slashCommands) {
-    const commandFiles = fs
-        .readdirSync('./commands/' + module)
-        .filter((file) => file.endsWith('.js'));
-
-    for (const commandFile of commandFiles) {
-        const command = require(`./commands/${module}/${commandFile}`);
-        client.slashCommands.set(command.name, command)
+    if (event.once) {
+        client.once(event.name, (...args) => event.run(...args, client))
+    } else {
+        client.on(event.name, (...args) => event.run(...args, client))
     };
 };
 
-// const buttonFiles = fs.readdirSync('./buttons');
+// Gets the slash command folder
+const slashCommands = fs.readdirSync('./commands')
 
-// for (const buttonFile of buttonFiles) {
-//     const button = require('./buttons/' + buttonFile)
-//     client.buttons.set(button.name, button)
-// };
+// Gets the commands from individual folder
+for (const module of slashCommands) {
+    const commandFiles = fs.readdirSync('./commands/' + module)
+        .filter((file) => file.endsWith('.js'));
 
-// init();
+    // Attaches the command to the client defined variable
+    for (const commandFile of commandFiles) {
+        const command = require(`./commands/${module}/${commandFile}`);
+        client.slashCommands.set(command.name, command);
+    };
+};
+
+// Initializes the database
+init()
+
+// Connects to the application
 client.login(process.env.TOKEN);
